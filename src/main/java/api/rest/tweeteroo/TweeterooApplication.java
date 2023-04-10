ee772d6c82c61e0143638de136ecf5e4ce89679a
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-04-10 10:21:30 -0300

    initial commit

---
+++ src/main/java/api/rest/tweeteroo/TweeterooApplication.java
@@ -0,0 +1,13 @@
+package api.rest.tweeteroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterooApplication.class, args);
+	}
+
+}
