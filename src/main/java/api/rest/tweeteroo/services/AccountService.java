f2c7f1637f5cfc6f842b4535174ce66b26f3eeca
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-04-10 11:29:38 -0300

    feat: implement account service

---
+++ src/main/java/api/rest/tweeteroo/services/AccountService.java
@@ -0,0 +1,26 @@
+package api.rest.tweeteroo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import api.rest.tweeteroo.dtos.AccountDTO;
+import api.rest.tweeteroo.models.Account;
+import api.rest.tweeteroo.repositories.AccountRepository;
+
+@Service
+public class AccountService {
+    
+    @Autowired
+    private AccountRepository repository;
+
+    public void create(@RequestBody AccountDTO dto) {
+        repository.save(new Account(dto));
+    }
+
+    public Optional<Account> getLoggedUser(Long id) {
+        return repository.findById(id);
+    }
+}
